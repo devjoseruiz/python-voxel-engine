f04329bd12a9d971d64fbdbaa30639ec0aa6390a
José Ruiz <61534214+devjoseruiz@users.noreply.github.com>
2023-10-30 23:13:43 +0100

    :sparkles: Generate a world of chunks

--- src/scene.py
+++ src/scene.py
@@ -1,14 +1,14 @@
 from settings import *
-from world_objects.chunk import Chunk
+from world import World
 
 
 class Scene:
     def __init__(self, app):
         self.app = app
-        self.chunk = Chunk(self.app)
+        self.world = World(self.app)
     
     def update(self):
-        pass
+        self.world.update()
 
     def render(self):
-        self.chunk.render()
+        self.world.render()
--- src/world_objects/chunk.py
+++ src/world_objects/chunk.py
@@ -3,16 +3,26 @@ from settings import *
 
 
 class Chunk:
-    def __init__(self, app):
-        self.app = app
-        self.voxels: np.array = self.build_voxels()
+    def __init__(self, world, position):
+        self.app = world.app
+        self.world = world
+        self.position = position
+        self.m_model = self.get_model_matrix()
+        self.voxels: np.array = None
         self.mesh: ChunkMesh = None
-        self.build_mesh()
+
+    def get_model_matrix(self):
+        m_model = glm.translate(glm.mat4(), glm.vec3(self.position) * CHUNK_SIZE)
+        return m_model
+
+    def set_uniform(self):
+        self.mesh.program['m_model'].write(self.m_model)
 
     def build_mesh(self):
         self.mesh = ChunkMesh(self)
 
     def render(self):
+        self.set_uniform()
         self.mesh.render()
 
     def build_voxels(self):
