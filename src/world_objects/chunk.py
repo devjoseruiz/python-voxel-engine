17556659c6c3b85285d79eeaff95be793d1d90aa
José Ruiz <61534214+devjoseruiz@users.noreply.github.com>
2023-10-30 23:18:53 +0100

    :construction: Implement basic terrain generation

--- src/world_objects/chunk.py
+++ src/world_objects/chunk.py
@@ -30,10 +30,16 @@ class Chunk:
         voxels = np.zeros(CHUNK_VOL, dtype='uint8')
 
         # fill chunk
+        cx, cy, cz = glm.ivec3(self.position) * CHUNK_SIZE
+
         for x in range(CHUNK_SIZE):
             for z in range(CHUNK_SIZE):
-                for y in range(CHUNK_SIZE):
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = (
-                        x + y + z if int(glm.simplex(glm.vec3(x, y, z) * 0.1) + 1) else 0
-                    )
+                wx = x + cx
+                wz = z + cz
+                world_height = int(glm.simplex(glm.vec2(wx, wz) * 0.01) * 32 + 32)
+                local_height = min(world_height - cy, CHUNK_SIZE)
+
+                for y in range(local_height):
+                    wy = y + cy
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = wy + 1
         return voxels
