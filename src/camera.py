3f91b9a0f5d7d286f37d42f1ce1088673e2ce90a
José Ruiz <61534214+devjoseruiz@users.noreply.github.com>
2023-11-05 04:36:14 +0100

    :bug: Fix camera orientation

--- src/camera.py
+++ src/camera.py
@@ -13,15 +13,13 @@ class Camera:
 
         self.m_proj = glm.perspective(V_FOV, ASPECT_RATIO, NEAR, FAR)
         self.m_view = glm.mat4()
-    
+
     def update(self):
         self.update_vectors()
         self.update_view_matrix()
 
     def update_view_matrix(self):
-        self.m_view = glm.lookAt(self.position,
-                                 self.position + self.forward,
-                                 self.up)
+        self.m_view = glm.lookAt(self.position, self.position + self.forward, self.up)
 
     def update_vectors(self):
         self.forward.x = glm.cos(self.yaw) * glm.cos(self.pitch)
@@ -35,24 +33,24 @@ class Camera:
     def rotate_pitch(self, delta_y):
         self.pitch -= delta_y
         self.pitch = glm.clamp(self.pitch, -PITCH_MAX, PITCH_MAX)
-    
+
     def rotate_yaw(self, delta_x):
         self.yaw += delta_x
-    
+
     def move_left(self, velocity):
         self.position -= self.right * velocity
-    
+
     def move_right(self, velocity):
         self.position += self.right * velocity
-    
+
     def move_up(self, velocity):
         self.position += self.up * velocity
-    
+
     def move_down(self, velocity):
         self.position -= self.up * velocity
 
     def move_forward(self, velocity):
         self.position += self.forward * velocity
-    
+
     def move_back(self, velocity):
         self.position -= self.forward * velocity
